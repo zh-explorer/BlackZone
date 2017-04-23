f28cb5904cccd14c467284c9c2bbf337b2ce4070
explorer <hsadkhk@gmail.com>
2017-04-23 23:11:31 +0800

    new dataDict

--- blackzone/container/dataDict.py
+++ blackzone/container/dataDict.py
@@ -1,13 +1,29 @@
 # coding=utf-8
 from collections import MutableMapping
+from json import dumps
 
 
 class DataDict(MutableMapping):
     """
     内部存放config元数据的类
     """
-    def __init__(self, **kw):
-        self.__dict__ = kw
+
+    def __init__(self, *args, **kwargs):
+        for d in args:
+            if not isinstance(d, dict):
+                raise KeyError
+            for key, value in d.items():
+                self.check(key,value)
+                if isinstance(value, dict):
+                    self.__dict__[key] = self.__class__(value)
+                else:
+                    self.__dict__[key] = value
+        for key, value in kwargs.items():
+            self.check(key, value)
+            if isinstance(value, dict):
+                self.__dict__[key] = self.__class__(value)
+            else:
+                self.__dict__[key] = value
 
     def __getitem__(self, key):
         return self.__dict__[key]
@@ -16,14 +32,25 @@ class DataDict(MutableMapping):
         del self.__dict__[key]
 
     def __setitem__(self, key, value):
-        #TODO 需要对添加对象的检查
-        self.__dict__[key] = value
+        self.check(key, value)
+        if isinstance(value, dict):
+            self.__dict__[key] = self.__class__(value)
+        else:
+            self.__dict__[key] = value
+
+    def __setattr__(self, key, value):
+        self[key] = value
 
     def keys(self):
         return self.__dict__.keys()
 
     def __len__(self):
         return len(self.__dict__)
+
+    def update(self, other):
+        if isinstance(other, self.__class__):
+            raise ValueError
+        self.__dict__.update(other)
 
     def __contains__(self, value):
         return value in self.__dict__
@@ -35,16 +62,35 @@ class DataDict(MutableMapping):
             return False
 
     def __ne__(self, other):
-        return not self.__dict__ == other.__dict__
+        return not self == other
 
     def copy(self):
-        return self.__class__(**self.__dict__)
+        return self.__class__(self.__dict__)
 
     def __iter__(self):
         return iter(self.__dict__)
 
     def __repr__(self):
-        return "%s(%s)" % (self.__class__.__name__, repr(self.__dict__))
+        return repr(self.__dict__)
 
     def __str__(self):
-        return "%s(%s)" % (self.__class__.__name__, str(self.__dict__))
+        return str(self.__dict__)
+
+        # def to_json(self):
+        #     dumps(self.to_dict())
+
+    def to_dict(self):
+        d = {}
+        for key,value in self.__dict__.items():
+            if isinstance(value, self.__class__):
+                d[key] = value.to_dict()
+            else:
+                d[key] = value
+        return d
+
+    def to_json(self):
+        return dumps(self.to_dict())
+
+    def check(self, key, value):
+        # TODO 需要对添加对象的检查
+        pass
