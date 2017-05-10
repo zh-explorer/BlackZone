67fdf3edd9967972a922a28bad7b879572cc46c6
explorer <explorer@hduisa.org>
2017-05-10 22:38:00 +0800

    write NetManager

--- blackzone/network/netManager.py
+++ blackzone/network/netManager.py
@@ -1,10 +1,11 @@
 from pyroute2 import IPDB
 from pyroute2 import IPRoute
+from pyroute2 import netns
 import os
 
 
 class NetManager(object):
-    def __init__(self, bridge_name="blackzone0", ip="172.20.0.1", mask=16):
+    def __init__(self, bridge_name="blackzone0", ip="172.20.0.1/16"):
         pid = os.getpid()
         if pid != 0:
             raise OSError("Permission denied: please use root")
@@ -20,7 +21,24 @@ class NetManager(object):
         # contention two
         ipdb.commit()     # reload info
         try:
-            if ipdb[bridge_name].kind == "bridge":
+            if ipdb.interfaces[bridge_name].kind == "bridge":
                 raise OSError("the bridge name is exist", bridge_name)
         except KeyError:
             raise OSError("This should not happen except contention, just retry")
+
+        # set bridge ip and up. The route should be add automatic
+        with ipdb.interfaces[bridge_name] as br:
+            br.down()
+
+        with ipdb.interfaces[bridge_name] as br:
+            br.add_ip(ip)
+            br.up()
+
+        self.ip = ip
+        self.bridge_name = bridge_name
+        self.ns_name = None
+
+    # it should be find to use contain_id as net namespace name.
+    def create_ns(self, ns_name):
+        self.ns_name = ns_name
+        netns.create(self.ns_name)
